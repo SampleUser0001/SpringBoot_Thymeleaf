a067a1af4f613bfb367686c6b6245eba945098a9
ittimfn <ittimfn+github@gmail.com>
2022-07-17 16:07:35 +0900

    tmp

--- README.md
+++ README.md
@@ -13,4 +13,6 @@ http://localhost:8080
 
 ## 参考
 
--[Thymeleaf Web 画面の作成:SpringBoot](https://spring.pleiades.io/guides/gs/serving-web-content/)
+- [Thymeleaf Web 画面の作成:SpringBoot](https://spring.pleiades.io/guides/gs/serving-web-content/)
+- [ThymeleafでListを繰り返して表示してみる。:プログラマーの調べもの](https://programmers-investigation.hatenablog.com/entry/2017/01/07/213158)
+- 
--- src/main/java/sample/webapp/thymeleaf/controller/GreetingController.java
+++ src/main/java/sample/webapp/thymeleaf/controller/GreetingController.java
@@ -5,13 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.ArrayList;
+
 @Controller
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	public String greeting(@RequestParam(name="nameList", required=false) List<String> nameList, Model model) {
+		nameList = new ArrayList<String>();
+		nameList.add("World");
+		nameList.add("Thymeleaf");
+		
+		model.addAttribute("nameList", nameList);
 		return "greeting";
 	}
+	
 
 }
